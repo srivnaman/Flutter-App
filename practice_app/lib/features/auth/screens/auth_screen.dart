d9adc3cad60689be4936cdcb5b73e52f77b49e1f
srivnaman <srinaman2@gmail.com>
2022-09-23 00:34:00 +0530

    edited Auth Screen

--- practice_app/lib/features/auth/screens/auth_screen.dart
+++ practice_app/lib/features/auth/screens/auth_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:practice_app/constants/global_variables.dart';
 
-enum Auth{
- signin,
- signup, 
+enum Auth {
+  signin,
+  signup,
 }
 
 class AuthScreen extends StatefulWidget {
-  static const String routeName='auth-screen';
+  static const String routeName = 'auth-screen';
   const AuthScreen({super.key});
 
   @override
@@ -17,44 +17,57 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   Auth _auth = Auth.signup;
 
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       backgroundColor: GlobalVariables.greyBackgroundCOlor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            
-            children: [
-              const Text("Welcome",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500
-
-              ),),
-              ListTile(
-                title: const Text("Create Account",
-                 style: TextStyle(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+            ),
+            //List Tile for Signin Radio Button
+            ListTile(
+              title: const Text(
+                "Create Account",
+                style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-
                 ),
-                ),
-                leading: Radio(
-                  activeColor: GlobalVariables.secondaryColor,
-                  value: Auth.signup, 
-                  groupValue: _auth,
-                  onChanged:((Auth? val) {
-                    _auth = val!;
-                  }) ,),
-
-              )
-            ],
+              ),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: Auth.signup,
+                groupValue: _auth,
+                onChanged: ((Auth? val) {
+                  _auth = val!;
+                }),
+              ),
             ),
-        ) ),
+            ListTile(
+              title: const Text(
+                "Sign-In Account",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: Auth.signin,
+                groupValue: _auth,
+                onChanged: ((Auth? val) {
+                  _auth = val!;
+                }),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
